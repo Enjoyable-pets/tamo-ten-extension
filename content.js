1b8021185067c5baa08773703f56b907f7c6b3d4
Kestutis Arlauskas <kestutisa@gmail.com>
2019-11-23 21:18:12 +0200

    manifest, content, jquery

---
+++ content.js
@@ -0,0 +1,3 @@
+$('#pagal_dienas div div.td div.row div.last-no-border div.td div.row div.text-center div').text(10);
+$('#pagal-dalykus div div.td div.row div.last-no-border div.td div.row div.text-center div').text(10);
+
